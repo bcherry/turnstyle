0b9d51929f1d0275297c8add36e59e1541c06658
Ben Cherry <bcherry@gmail.com>
2009-09-19 13:11:04 -0700

    fixed links to script/style

--- turnstyle.js
+++ turnstyle.js
@@ -1,14 +1,12 @@
 (function() {
-	var dir = '';
 	var loadedFiles = {};
-	this.$cssDir = function(newDir) { dir = newDir; };
 	this.$css = function(filename) {
 		if (loadedFiles[filename]) { 
 			return;
 		}
 		loadedFiles[filename] = true;
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", dir + filename, true);
+		xhr.open("GET",filename, true);
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState === 4) {
 				var head = document.getElementsByTagName("head")[0];
