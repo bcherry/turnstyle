a4d99beff10340c53741298f117d1d524eb7a393
Ben Cherry <bcherry@gmail.com>
2009-09-20 18:27:45 -0700

    preliminary css mod support

--- jquery.turnstyle.js
+++ jquery.turnstyle.js
@@ -1,25 +1,33 @@
 (function() {
 	var loadedFiles = {};
-	jQuery.css = function(filename) {
-		if (loadedFiles[filename]) { 
-			return;
+	jQuery.css = function(filename, obj) {
+		if (typeof obj == "undefined") {
+			if (loadedFiles[filename]) { 
+				return;
+			}
+			loadedFiles[filename] = true;
 		}
-		loadedFiles[filename] = true;
 		jQuery.get(filename, function(data) {
 			var head = document.getElementsByTagName("head")[0];
 			var styleTag = document.createElement("style");
-			var style = document.createTextNode(data);
+			var text = data;
+			console.log(text);
+			if (typeof obj != "undefined") {
+				var re = /\s*([a-z0-9\.# -_:@]+\s*{)/gim;
+				text = text.replace(re, '\n.test $1');
+				console.log(text);
+			}
+			var style = document.createTextNode(text);
 			styleTag.appendChild(style);
 			head.appendChild(styleTag);
 		});
 	};
 	var _super_css = jQuery.fn.css;
 	jQuery.fn.css = function(arg1, arg2) {
-		console.log(arg1 + " " + arg2);
 		if (typeof arg1 !== "undefined" && typeof arg1.filename == "string") {
-			jQuery.css(arg1.filename);
+			jQuery.css(arg1.filename, this);
 		} else if (arg1 == "filename") {
-			jQuery.css(arg2);
+			jQuery.css(arg2, this);
 			return;
 		}
 		return _super_css.apply(this, Array.prototype.slice.apply(arguments));
