a3ba782c4ddd131e69d7b13703b20911afcc0d5e
Ben Cherry <bcherry@gmail.com>
2009-09-21 11:32:25 -0700

    added support for direct text rules

--- demo.js
+++ demo.js
@@ -1,6 +1,10 @@
 $(function() {
-	$("#test1").click(function() {
+	$("#demo1").click(function() {
 		$(this).css("filename","test.css");
+	});
+
+	$("#demo2").click(function() {
+		$(this).css("rules",$(this).find(".style").text());
 	});
 
 	$("#header").click(function() {
--- jquery.turnstyle.js
+++ jquery.turnstyle.js
@@ -1,28 +1,35 @@
 (function() {
 	var loadedFiles = {};
 	var seed = 0;
+	var re = /\s*([a-z0-9\.# -_:@>]+\s*{)/gim;
+	var loadStyle = function(text, obj) {
+		var head = document.getElementsByTagName("head")[0];
+		var styleTag = document.createElement("style");
+		console.log(text);
+		if (typeof obj != "undefined") {
+			var namespace = "__css" + seed++;
+			text = text.replace(re, '\n.' + namespace + ' $1');
+			obj.addClass(namespace);
+			console.log(text);
+		}
+		var style = document.createTextNode(text);
+		styleTag.appendChild(style);
+		head.appendChild(styleTag);
+	}
 	jQuery.css = function(filename, obj) {
+		if (filename.match(re)) {
+			loadStyle(filename, obj);
+			return;
+		}
 		if (typeof obj == "undefined") {
 			if (loadedFiles[filename]) { 
 				return;
 			}
 			loadedFiles[filename] = true;
 		}
+
 		jQuery.get(filename, function(data) {
-			var head = document.getElementsByTagName("head")[0];
-			var styleTag = document.createElement("style");
-			var text = data;
-			console.log(text);
-			if (typeof obj != "undefined") {
-				var re = /\s*([a-z0-9\.# -_:@]+\s*{)/gim;
-				var namespace = "__css" + seed++;
-				text = text.replace(re, '\n.' + namespace + ' $1');
-				obj.addClass(namespace);
-				console.log(text);
-			}
-			var style = document.createTextNode(text);
-			styleTag.appendChild(style);
-			head.appendChild(styleTag);
+			loadStyle(data,obj);
 		});
 	};
 	var _super_css = jQuery.fn.css;
@@ -32,6 +39,8 @@
 		} else if (arg1 == "filename") {
 			jQuery.css(arg2, this);
 			return;
+		} else if (arg1 == "rules") {
+			jQuery.css(arg2, this);
 		}
 		return _super_css.apply(this, Array.prototype.slice.apply(arguments));
 	};
