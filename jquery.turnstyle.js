7fc2aedb9fda36cd8990d6ece307446a3e0d7502
Ben Cherry <bcherry@gmail.com>
2009-09-21 23:58:33 -0700

    restructured code and added comments, MIT license

--- jquery.turnstyle.js
+++ jquery.turnstyle.js
@@ -1,47 +1,101 @@
-(function() {
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009 Ben Cherry (bcherry@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+(function($) {
+	// Local variables
 	var loadedFiles = {};
 	var seed = 0;
 	var re = /\s*([a-z0-9\.# -_:@>]+\s*{)/gim;
+
+	// Loads the specified CSS onto the page or optional object
 	var loadStyle = function(text, obj) {
 		var head = document.getElementsByTagName("head")[0];
 		var styleTag = document.createElement("style");
-		console.log(text);
+
+		// Namespace our object and the CSS
 		if (typeof obj != "undefined") {
 			var namespace = "__css" + seed++;
 			text = text.replace(re, '\n.' + namespace + ' $1');
 			obj.addClass(namespace);
-			console.log(text);
 		}
+
 		var style = document.createTextNode(text);
 		styleTag.appendChild(style);
 		head.appendChild(styleTag);
 	}
-	jQuery.css = function(filename, obj) {
+
+	// Static function to load CSS
+	$.css = function(filename, obj) {
+
+		// Loading rules directly
 		if (filename.match(re)) {
 			loadStyle(filename, obj);
 			return;
 		}
-		if (typeof obj == "undefined") {
-			if (loadedFiles[filename]) { 
+
+		// Check the cache
+		if (loadedFiles[filename]) { 
+			// Static file, don't load again
+			if (typeof obj == "undefined") {
+				return;
+			// Namespaced, but we already have the stylesheet
+			} else {
+				loadStyle(loadedFiles[filename], obj);
 				return;
 			}
-			loadedFiles[filename] = true;
 		}
 
-		jQuery.get(filename, function(data) {
-			loadStyle(data,obj);
+		// Request the file
+		$.get(filename, function(data) {
+			loadedFiles[filename] = data;
+			loadStyle(data, obj);
 		});
 	};
-	var _super_css = jQuery.fn.css;
-	jQuery.fn.css = function(arg1, arg2) {
-		if (typeof arg1 !== "undefined" && typeof arg1.filename == "string") {
-			jQuery.css(arg1.filename, this);
-		} else if (arg1 == "filename") {
-			jQuery.css(arg2, this);
-			return;
+
+	// We override jQuery.fn.css, so keep the old one around
+	var _super_css = $.fn.css;
+
+	// Extended jQuery.fn.css
+	$.fn.css = function(arg1, arg2) {
+		// Load a file onto this object
+		if (arg1 == "filename") {
+			$.css(arg2, this);
+		// Load rules onto this object
 		} else if (arg1 == "rules") {
-			jQuery.css(arg2, this);
+			$.css(arg2, this);
+		// Passed a dict, multiple options
+		} else if (typeof arg1 !== "undefined") {
+			// Load a file onto this object
+			if (typeof arg1.filename == "string") {
+				$.css(arg1.filename, this);
+			}
+			// Load rules onto this object
+			if (typeof arg1.rules == "string") {
+				$.css(arg1.rules, this);
+			}
 		}
+
+		// Apply the original CSS function, to preserve chaining and existing CSS extension plugins
 		return _super_css.apply(this, Array.prototype.slice.apply(arguments));
 	};
-})();
+})(jQuery);
